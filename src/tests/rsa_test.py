417e3c4c7bafca025c17d0552a898393f16a74b4
Joona <joona.t.pasanen@gmail.com>
2026-06-03 08:28:02 +0300

    added unit tests for generate keys function

--- src/tests/rsa_test.py
+++ src/tests/rsa_test.py
@@ -5,10 +5,26 @@ class TestExtendedEuclidianAlgorithm(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_gcd_784_and_12056(self):
+    def test_gcd_784_and_12056_bezout_coefficient_1(self):
+        self.assertEqual(692, extended_euclidian_algorithm(784, 12056)[0])
+    
+    def test_gcd_784_and_12056_bezout_coefficient_2(self):
+        self.assertEqual(-45, extended_euclidian_algorithm(784, 12056)[1])
+
+    def test_gcd_784_and_12056_gcd(self):
         self.assertEqual(8, extended_euclidian_algorithm(784, 12056)[2])
 
 class TestGenerateKeys(unittest.TestCase):
     def setUp(self):
-        pass
-    
+        self.public_key, self.private_key = generate_keys()
+
+    def test_public_key_tuple_length(self):
+        public_key_len = len(self.public_key)
+        self.assertEqual(public_key_len, 2)
+
+    def test_private_key_tuple_length(self):
+        public_key_len = len(self.public_key)
+        self.assertEqual(public_key_len, 2)
+
+    def test_keys_same_N(self):
+        self.assertEqual(self.public_key[0], self.private_key[0])
